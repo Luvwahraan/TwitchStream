f90898a36592ffd41b33b65b4cd5b5121794aec9
Luvwahraan <luvwahraan@pikaed.org>
2024-11-07 02:57:09 +0100

    Better launching script

--- prepare.sh
+++ prepare.sh
@@ -6,14 +6,15 @@
 
 TStream=/home/luvwahraan/TwitchStream
 
-Disk=${TStream}/data
-RP=${Disk}/roon_playing
-NP=${Disk}/now.playing
-LOCK_FILE=${Disk}/roon_np.lock
+DISK=${TStream}/data
+RP=${DISK}/roon_playing
+NP=${DISK}/now.playing
+LOCK_FILE=${DISK}/roon_np.lock
 
 BG=${TStream}/backgrounds
-NIMG=${Disk}/background.jpg
+NIMG=${DISK}/background.jpg
 
+echo "Copy a random stream background."
 cp ${BG}/$(ls "${BG}" | sort -R | tail -n1) ${NIMG}
 
 ROON=/usr/local/Roon
@@ -22,18 +23,27 @@ ROONETC=${ROON}/etc
 ROONCONF=${ROONETC}/pyroonconf
 NOWP=now_playing.py
 
-#cd ${ROONAPI} || exit 1
-
 # Use a Python virtual environment
 [ -f ${ROON}/venv/bin/activate ] && source ${ROON}/venv/bin/activate
 [[ ":$PATH:" == *":/usr/local/Roon/venv/bin:"* ]] || {
   export PATH=/usr/local/Roon/venv/bin:${PATH}
 }
 
+echo "Reset counters."
 echo '' > ${NP}
 touch ${RP}
 
+echo "Alsa loopback"
+screen -dmS alsaloop alsaloop -r 48000 -C hw:0 -P hw:0 -l 256 -s 0 -U
+echo $! > ${DISK}/loopback.pid
 
+echo "Roon playback."
+roon -z 'Roon Server' -c play
+
+echo "Roon now playing data."
 screen -dmS ${TStream}/rn_loop.sh
-screen -dmS ${TStream}/rec_loop.sh
+echo $! > ${DISK}/now_playing.pid
 
+echo "ffmpeg stream"
+screen -dmS ${TStream}/rec_loop.sh
+echo $! > ${DISK}/stream.pid
