a85e22dd3c0ea266a9790768bd7b50416548a8fd
Luvwahraan <luvwahraan@pikaed.org>
2024-11-07 02:56:50 +0100

    Show stream crashes counter

--- rec_loop.sh
+++ rec_loop.sh
@@ -6,9 +6,13 @@
 TStream=/home/luvwahraan/TwitchStream
 NPLAY=$TStream/data/now_playing
 LOCK_FILE=${TStream}/data/roon_np.lock
+NB=${TStream}/data/count.np
 
 NIMG=${TStream}/data/background.jpg
 BG_DIR=${TStream}/backgrounds
+
+echo 1 > $LOCK_FILE
+echo -e "0\n0" > $NB
 
 trap "echo 0 > $LOCK_FILE ; exit 0" \
   HUP INT QUIT EXIT KILL TERM #1 2 3 9 15
@@ -16,11 +20,14 @@ trap "echo 0 > $LOCK_FILE ; exit 0" \
 CRASHED=0
 while [ $(head -n1 $LOCK_FILE) -eq 1 ] ; do
   echo -n "Crash: ${CRASHED}"
+  
+  UPDATED=$(head -n1 $NB)
+  echo -e "${UPDATED}\n${CRASHED}" > $NB
 
   # Change background
   cp ${BG_DIR}/$(ls "${BG_DIR}" | sort -R | tail -n1) ${NIMG}
   
-  ${TStream}/stream.py -d "${TStream}" --stream
+  ${TStream}/stream.py -d "${TStream}" --stream --verbose
   CRASHED=$(( $CRACHED + 1 ))
   sleep 1
 done
--- rn_loop.sh
+++ rn_loop.sh
@@ -11,9 +11,12 @@ NP=${Disk}/now.playing
 NP=${Disk}/last.np
 NB=${Disk}/count.np
 
+LOCK_FILE=${Disk}/roon_np.lock
+echo 1 > "$LOCK_FILE"
+
+STREAM_CRASH=$(tail -n1 $NB)
 WAIT=10
 
-LOCK_FILE=${Disk}/roon_np.lock
 PID=''
 
 trap "echo 0 > $LOCK_FILE ; echo '' > $NP ; kill $PID ; exit 0" \
@@ -22,22 +25,25 @@ trap "echo 0 > $LOCK_FILE ; echo '' > $NP ; kill $PID ; exit 0" \
 counter() {
   i=0
   while [ $(head -n1 $LOCK_FILE) -eq 1 ] ; do
-    echo $i > $NB
+    echo -e "$i\n${STREAM_CRASH}" > $NB
     i=$(( $i + 1 ))
     sleep 1
   done
 }
 
-
-echo 1 > "$LOCK_FILE"
 LAST_PID=0
 while [ $(head -n1 $LOCK_FILE) -eq 1 ] ; do
   echo -n '.'
 
   ${TStream}/roon_now.sh 'Roon Server' "${TStream}"
 
-  # Kill last counter, and start a new one.
+  # Kill last counter
   kill $PID
+  
+  # Update stream crashed count
+  STREAM_CRASH=$(tail -n1 $NB)
+  
+  # Start a new last updated counter.
   counter &
   PID=$!
 
